ff5dc520d8daa2450fe63a1b60b293705e863447
Shinhuichan <94081602shc@gmail.com>
2025-07-04 17:06:22 +0900

    v0.0.10

---
+++ Assets/FluidSim/Examples/ExampleFiles/rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    public float rotateSpeed = 1.0f;
+
+    private Vector3 rotateVector = Vector3.zero;
+
+    private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rotateVector.z = rotateSpeed * Time.deltaTime;
+
+        myTransform.Rotate(rotateVector);
+    }
+}
